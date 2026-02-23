2d582e8c83c0797c3be4947872799da110c8c9c0
Max Kircher <max@cronologic.de>
2026-02-23 10:36:22 +0100

    update add_colorbar example

--- docs/source/_examples/add_colorbar.py
+++ docs/source/_examples/add_colorbar.py
@@ -8,14 +8,14 @@ fig, axs = plt.subplots(1, 2, layout=mplu.FixedLayoutEngine())
 
 image = np.random.rand(10, 10)
 
-axs[0].set_title("Added after resizing")
 im0 = axs[0].imshow(image)
-mplu.set_axes_size(2.5, ax=axs[0])
-mplu.add_colorbar(im0, ax=axs[0], thickness_pts=8.0)
-
-axs[1].set_title("Added before resizing")
 im1 = axs[1].imshow(image)
-mplu.add_colorbar(im1, ax=axs[1], thickness_pts=8.0)
-mplu.set_axes_size(2.5, ax=axs[1])
 
-plt.show()
+mplu.add_colorbar(im0, ax=axs[0])
+mplu.add_colorbar(im1, ax=axs[1], thickness=8.0, pad=4.0, location="top")
+
+for ax in axs:
+    mplu.set_axes_size(2.5, ax=ax)
+
+# the colorbar placement shifts the axes position, we need to realign it
+mplu.align_axes_vertically(*axs, alignment="bottom")
